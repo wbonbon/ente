0343eb808675de000d5357dae09f954395a737c8
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-06-11 12:27:01 +0530

    Document

--- mobile/lib/services/filter/filter.dart
+++ mobile/lib/services/filter/filter.dart
@@ -1,5 +1,6 @@
 import 'package:photos/models/file/file.dart';
 
 abstract class Filter {
+  // True value indicates that the file passes the filter, and we should keep it
   bool filter(EnteFile file);
 }
