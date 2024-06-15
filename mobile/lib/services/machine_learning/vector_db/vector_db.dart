d02170f87fa39b72d2ab18f38cd2cfb4872b923e
laurenspriem <github_uni.fishers@aleeas.com>
2024-06-15 12:33:55 +0530

    [mob][photos] Basic vectorDB API

---
+++ mobile/lib/services/machine_learning/vector_db/vector_db.dart
@@ -0,0 +1,247 @@
+import "dart:typed_data" show Float32List;
+
+import "package:computer/computer.dart" show Computer;
+import "package:flutter/foundation.dart" show kDebugMode;
+import "package:flutter_rust_bridge/flutter_rust_bridge.dart" show Uint64List;
+import "package:logging/logging.dart" show Logger;
+import "package:path/path.dart" show join;
+import "package:path_provider/path_provider.dart"
+    show getApplicationDocumentsDirectory;
+import "package:photos/extensions/stop_watch.dart" show EnteWatch;
+import "package:photos/src/rust/api/usearch_api.dart" as rust;
+import "package:synchronized/synchronized.dart" show Lock;
+
+enum VectorDbTable {
+  faceMl,
+  clip,
+}
+
+final _faceMlLock = Lock();
+final _clipLock = Lock();
+
+class VectorDB {
+  final _logger = Logger("VectorDB");
+
+  final _computer = Computer.shared();
+
+  final Map<VectorDbTable, String> _tablePaths = {};
+
+  // Singleton pattern
+  VectorDB._privateConstructor();
+  static final instance = VectorDB._privateConstructor();
+  factory VectorDB() => instance;
+
+  Future<String> getIndexPath(VectorDbTable table) async {
+    _tablePaths[table] ??= await _initVectorDbTable(table);
+    return _tablePaths[table]!;
+  }
+
+  Future<String> _initVectorDbTable(VectorDbTable table) async {
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    final String tableDirectory =
+        join(documentsDirectory.path, _vectorDbTablePath(table));
+    return tableDirectory;
+  }
+
+  String _vectorDbTablePath(VectorDbTable table) {
+    switch (table) {
+      case VectorDbTable.faceMl:
+        return "face_ml_index.usi";
+      case VectorDbTable.clip:
+        return "clip_index.usi";
+    }
+  }
+
+  Lock _vectorDbLock(VectorDbTable table) {
+    switch (table) {
+      case VectorDbTable.faceMl:
+        return _faceMlLock;
+      case VectorDbTable.clip:
+        return _clipLock;
+    }
+  }
+
+  Future<(int, int, int)> getIndexStatus(
+    VectorDbTable table,
+  ) async {
+    _logger.info("getIndexStatus called for $table");
+    return await _vectorDbLock(table).synchronized(() async {
+      final w = (kDebugMode ? EnteWatch('getIndexStatus for $table') : null)
+        ?..start();
+      final stats = await _computer.compute(
+        _getIndexStatus,
+        param: {
+          "indexPath": getIndexPath(table),
+        },
+        taskName: "getIndexStatus in $table",
+      ) as (int, int, int);
+      w?.log('getIndexStatus for $table');
+      return (stats.$1, stats.$2, stats.$3);
+    });
+  }
+
+  static Future<(int, int, int, int, int)> _getIndexStatus(Map args) async {
+    final String indexPath = args["indexPath"];
+    final (size, capacity, dimensions, expansionAdd, expansionSearch) =
+        await rust.getIndexStats(indexPath: indexPath);
+    return (
+      size.toInt(),
+      capacity.toInt(),
+      dimensions.toInt(),
+      expansionAdd.toInt(),
+      expansionSearch.toInt()
+    );
+  }
+
+  Future<void> addVector(
+    VectorDbTable table,
+    int key,
+    List<double> vector,
+  ) async {
+    _logger.info("addVector called for $table");
+    await _vectorDbLock(table).synchronized(() async {
+      final w = (kDebugMode ? EnteWatch('addVector for $table') : null)
+        ?..start();
+      await _computer.compute(
+        _addVector,
+        param: {
+          "indexPath": getIndexPath(table),
+          "key": key,
+          "vector": vector,
+        },
+        taskName: "addVector in $table",
+      );
+      w?.log('addVector for $table');
+    });
+  }
+
+  static Future<void> _addVector(Map args) async {
+    final String indexPath = args["indexPath"];
+    final BigInt key = BigInt.from(args["key"] as int);
+    final List<double> vector = args["vector"];
+    await rust.addVector(indexPath: indexPath, key: key, vector: vector);
+  }
+
+  Future<void> bulkAddVectors(
+    VectorDbTable table,
+    List<int> keys,
+    List<List<double>> vectors,
+  ) async {
+    _logger.info("bulkAddVectors called for $table");
+    await _vectorDbLock(table).synchronized(() async {
+      final w = (kDebugMode ? EnteWatch('bulkAddVectors for $table') : null)
+        ?..start();
+      await _computer.compute(
+        _bulkAddVectors,
+        param: {
+          "indexPath": getIndexPath(table),
+          "vectors": vectors,
+        },
+        taskName: "bulkAddVectors in $table",
+      );
+      w?.log('bulkAddVectors in $table');
+    });
+  }
+
+  static Future<void> _bulkAddVectors(Map args) async {
+    final String indexPath = args["indexPath"];
+    final List<List<double>> vectorsRaw = args["vectors"];
+    final Uint64List keys = Uint64List.fromList(args["keys"] as List<int>);
+    final List<Float32List> vectors =
+        vectorsRaw.map((e) => Float32List.fromList(e)).toList();
+    await rust.bulkAddVectors(
+      indexPath: indexPath,
+      keys: keys,
+      vectors: vectors,
+    );
+  }
+
+  Future<(Uint64List, Float32List)> searchVectors(
+    VectorDbTable table,
+    List<double> query,
+    int count,
+  ) async {
+    _logger.info("searchVectors called for $table");
+    return await _vectorDbLock(table).synchronized(() async {
+      final w = (kDebugMode ? EnteWatch('searchVectors for $table') : null)
+        ?..start();
+      final (Uint64List, Float32List) results = await _computer.compute(
+        _searchVectors,
+        param: {
+          "indexPath": getIndexPath(table),
+          "query": query,
+          "count": count,
+        },
+        taskName: "searchVectors in $table",
+      );
+      w?.log('searchVectors for $table');
+      return results;
+    });
+  }
+
+  static Future<(Uint64List, Float32List)> _searchVectors(Map args) async {
+    final String indexPath = args["indexPath"];
+    final List<double> query = args["query"];
+    final BigInt count = BigInt.from(args["count"] as int);
+    final (Uint64List keys, Float32List distances) = await rust.searchVectors(
+      indexPath: indexPath,
+      query: query,
+      count: count,
+    );
+    return (keys, distances);
+  }
+
+  Future<int> removeVector(
+    VectorDbTable table,
+    int key,
+  ) async {
+    _logger.info("removeVector called for $table");
+    return await _vectorDbLock(table).synchronized(() async {
+      final w = (kDebugMode ? EnteWatch('removeVector for $table') : null)
+        ?..start();
+      final int removedCount = await _computer.compute(
+        _removeVector,
+        param: {
+          "indexPath": getIndexPath(table),
+          "key": key,
+        },
+        taskName: "removeVector in $table",
+      );
+      w?.log('removeVector for $table');
+      return removedCount;
+    });
+  }
+
+  static Future<int> _removeVector(Map args) async {
+    final String indexPath = args["indexPath"];
+    final BigInt key = BigInt.from(args["key"] as int);
+    final removedCount =
+        await rust.removeVector(indexPath: indexPath, key: key);
+    assert(removedCount.isValidInt);
+    return removedCount.toInt();
+  }
+
+  /// WARNING: This will remove all vectors from the index, use with caution.
+  Future<void> resetIndex(
+    VectorDbTable table,
+  ) async {
+    _logger.info("resetIndex called for $table");
+    await _vectorDbLock(table).synchronized(() async {
+      final w = (kDebugMode ? EnteWatch('resetIndex for $table') : null)
+        ?..start();
+      await _computer.compute(
+        _resetIndex,
+        param: {
+          "indexPath": getIndexPath(table),
+        },
+        taskName: "resetIndex in $table",
+      );
+      w?.log('resetIndex for $table');
+    });
+  }
+
+  static Future<void> _resetIndex(Map args) async {
+    final String indexPath = args["indexPath"];
+    await rust.resetIndex(indexPath: indexPath);
+  }
+}
--- mobile/lib/src/rust/frb_generated.dart
+++ mobile/lib/src/rust/frb_generated.dart
@@ -4,6 +4,7 @@
 // ignore_for_file: unused_import, unused_element, unnecessary_import, duplicate_ignore, invalid_use_of_internal_member, annotate_overrides, non_constant_identifier_names, curly_braces_in_flow_control_structures, prefer_const_literals_to_create_immutables, unused_field
 
 import 'api/simple.dart';
+import 'api/usearch_api.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'frb_generated.dart';
@@ -57,7 +58,7 @@ class RustLib extends BaseEntrypoint<RustLibApi, RustLibApiImpl, RustLibWire> {
   String get codegenVersion => '2.0.0-dev.39';
 
   @override
-  int get rustContentHash => -1918914929;
+  int get rustContentHash => 2006111302;
 
   static const kDefaultExternalLibraryLoaderConfig =
       ExternalLibraryLoaderConfig(
@@ -71,6 +72,32 @@ abstract class RustLibApi extends BaseApi {
   String crateApiSimpleGreet({required String name});
 
   Future<void> crateApiSimpleInitApp();
+
+  Future<void> crateApiUsearchApiAddVector(
+      {required String indexPath,
+      required BigInt key,
+      required List<double> vector});
+
+  Future<void> crateApiUsearchApiBulkAddVectors(
+      {required String indexPath,
+      required Uint64List keys,
+      required List<Float32List> vectors});
+
+  Future<(BigInt, BigInt, BigInt, BigInt, BigInt)>
+      crateApiUsearchApiGetIndexStats({required String indexPath});
+
+  Future<Float32List> crateApiUsearchApiGetVector(
+      {required String indexPath, required BigInt key});
+
+  Future<BigInt> crateApiUsearchApiRemoveVector(
+      {required String indexPath, required BigInt key});
+
+  Future<void> crateApiUsearchApiResetIndex({required String indexPath});
+
+  Future<(Uint64List, Float32List)> crateApiUsearchApiSearchVectors(
+      {required String indexPath,
+      required List<double> query,
+      required BigInt count});
 }
 
 class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
@@ -127,6 +154,202 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         argNames: [],
       );
 
+  @override
+  Future<void> crateApiUsearchApiAddVector(
+      {required String indexPath,
+      required BigInt key,
+      required List<double> vector}) {
+    return handler.executeNormal(NormalTask(
+      callFfi: (port_) {
+        final serializer = SseSerializer(generalizedFrbRustBinding);
+        sse_encode_String(indexPath, serializer);
+        sse_encode_u_64(key, serializer);
+        sse_encode_list_prim_f_32_loose(vector, serializer);
+        pdeCallFfi(generalizedFrbRustBinding, serializer,
+            funcId: 3, port: port_);
+      },
+      codec: SseCodec(
+        decodeSuccessData: sse_decode_unit,
+        decodeErrorData: null,
+      ),
+      constMeta: kCrateApiUsearchApiAddVectorConstMeta,
+      argValues: [indexPath, key, vector],
+      apiImpl: this,
+    ));
+  }
+
+  TaskConstMeta get kCrateApiUsearchApiAddVectorConstMeta =>
+      const TaskConstMeta(
+        debugName: "add_vector",
+        argNames: ["indexPath", "key", "vector"],
+      );
+
+  @override
+  Future<void> crateApiUsearchApiBulkAddVectors(
+      {required String indexPath,
+      required Uint64List keys,
+      required List<Float32List> vectors}) {
+    return handler.executeNormal(NormalTask(
+      callFfi: (port_) {
+        final serializer = SseSerializer(generalizedFrbRustBinding);
+        sse_encode_String(indexPath, serializer);
+        sse_encode_list_prim_u_64_strict(keys, serializer);
+        sse_encode_list_list_prim_f_32_strict(vectors, serializer);
+        pdeCallFfi(generalizedFrbRustBinding, serializer,
+            funcId: 4, port: port_);
+      },
+      codec: SseCodec(
+        decodeSuccessData: sse_decode_unit,
+        decodeErrorData: null,
+      ),
+      constMeta: kCrateApiUsearchApiBulkAddVectorsConstMeta,
+      argValues: [indexPath, keys, vectors],
+      apiImpl: this,
+    ));
+  }
+
+  TaskConstMeta get kCrateApiUsearchApiBulkAddVectorsConstMeta =>
+      const TaskConstMeta(
+        debugName: "bulk_add_vectors",
+        argNames: ["indexPath", "keys", "vectors"],
+      );
+
+  @override
+  Future<(BigInt, BigInt, BigInt, BigInt, BigInt)>
+      crateApiUsearchApiGetIndexStats({required String indexPath}) {
+    return handler.executeNormal(NormalTask(
+      callFfi: (port_) {
+        final serializer = SseSerializer(generalizedFrbRustBinding);
+        sse_encode_String(indexPath, serializer);
+        pdeCallFfi(generalizedFrbRustBinding, serializer,
+            funcId: 5, port: port_);
+      },
+      codec: SseCodec(
+        decodeSuccessData: sse_decode_record_usize_usize_usize_usize_usize,
+        decodeErrorData: null,
+      ),
+      constMeta: kCrateApiUsearchApiGetIndexStatsConstMeta,
+      argValues: [indexPath],
+      apiImpl: this,
+    ));
+  }
+
+  TaskConstMeta get kCrateApiUsearchApiGetIndexStatsConstMeta =>
+      const TaskConstMeta(
+        debugName: "get_index_stats",
+        argNames: ["indexPath"],
+      );
+
+  @override
+  Future<Float32List> crateApiUsearchApiGetVector(
+      {required String indexPath, required BigInt key}) {
+    return handler.executeNormal(NormalTask(
+      callFfi: (port_) {
+        final serializer = SseSerializer(generalizedFrbRustBinding);
+        sse_encode_String(indexPath, serializer);
+        sse_encode_u_64(key, serializer);
+        pdeCallFfi(generalizedFrbRustBinding, serializer,
+            funcId: 6, port: port_);
+      },
+      codec: SseCodec(
+        decodeSuccessData: sse_decode_list_prim_f_32_strict,
+        decodeErrorData: null,
+      ),
+      constMeta: kCrateApiUsearchApiGetVectorConstMeta,
+      argValues: [indexPath, key],
+      apiImpl: this,
+    ));
+  }
+
+  TaskConstMeta get kCrateApiUsearchApiGetVectorConstMeta =>
+      const TaskConstMeta(
+        debugName: "get_vector",
+        argNames: ["indexPath", "key"],
+      );
+
+  @override
+  Future<BigInt> crateApiUsearchApiRemoveVector(
+      {required String indexPath, required BigInt key}) {
+    return handler.executeNormal(NormalTask(
+      callFfi: (port_) {
+        final serializer = SseSerializer(generalizedFrbRustBinding);
+        sse_encode_String(indexPath, serializer);
+        sse_encode_u_64(key, serializer);
+        pdeCallFfi(generalizedFrbRustBinding, serializer,
+            funcId: 7, port: port_);
+      },
+      codec: SseCodec(
+        decodeSuccessData: sse_decode_usize,
+        decodeErrorData: null,
+      ),
+      constMeta: kCrateApiUsearchApiRemoveVectorConstMeta,
+      argValues: [indexPath, key],
+      apiImpl: this,
+    ));
+  }
+
+  TaskConstMeta get kCrateApiUsearchApiRemoveVectorConstMeta =>
+      const TaskConstMeta(
+        debugName: "remove_vector",
+        argNames: ["indexPath", "key"],
+      );
+
+  @override
+  Future<void> crateApiUsearchApiResetIndex({required String indexPath}) {
+    return handler.executeNormal(NormalTask(
+      callFfi: (port_) {
+        final serializer = SseSerializer(generalizedFrbRustBinding);
+        sse_encode_String(indexPath, serializer);
+        pdeCallFfi(generalizedFrbRustBinding, serializer,
+            funcId: 8, port: port_);
+      },
+      codec: SseCodec(
+        decodeSuccessData: sse_decode_unit,
+        decodeErrorData: null,
+      ),
+      constMeta: kCrateApiUsearchApiResetIndexConstMeta,
+      argValues: [indexPath],
+      apiImpl: this,
+    ));
+  }
+
+  TaskConstMeta get kCrateApiUsearchApiResetIndexConstMeta =>
+      const TaskConstMeta(
+        debugName: "reset_index",
+        argNames: ["indexPath"],
+      );
+
+  @override
+  Future<(Uint64List, Float32List)> crateApiUsearchApiSearchVectors(
+      {required String indexPath,
+      required List<double> query,
+      required BigInt count}) {
+    return handler.executeNormal(NormalTask(
+      callFfi: (port_) {
+        final serializer = SseSerializer(generalizedFrbRustBinding);
+        sse_encode_String(indexPath, serializer);
+        sse_encode_list_prim_f_32_loose(query, serializer);
+        sse_encode_usize(count, serializer);
+        pdeCallFfi(generalizedFrbRustBinding, serializer,
+            funcId: 9, port: port_);
+      },
+      codec: SseCodec(
+        decodeSuccessData:
+            sse_decode_record_list_prim_u_64_strict_list_prim_f_32_strict,
+        decodeErrorData: null,
+      ),
+      constMeta: kCrateApiUsearchApiSearchVectorsConstMeta,
+      argValues: [indexPath, query, count],
+      apiImpl: this,
+    ));
+  }
+
+  TaskConstMeta get kCrateApiUsearchApiSearchVectorsConstMeta =>
+      const TaskConstMeta(
+        debugName: "search_vectors",
+        argNames: ["indexPath", "query", "count"],
+      );
+
   @protected
   String dco_decode_String(dynamic raw) {
     // Codec=Dco (DartCObject based), see doc to use other codecs
@@ -134,9 +357,80 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   }
 
   @protected
+  double dco_decode_f_32(dynamic raw) {
+    // Codec=Dco (DartCObject based), see doc to use other codecs
+    return raw as double;
+  }
+
+  @protected
+  List<Float32List> dco_decode_list_list_prim_f_32_strict(dynamic raw) {
+    // Codec=Dco (DartCObject based), see doc to use other codecs
+    return (raw as List<dynamic>)
+        .map(dco_decode_list_prim_f_32_strict)
+        .toList();
+  }
+
+  @protected
+  List<double> dco_decode_list_prim_f_32_loose(dynamic raw) {
+    // Codec=Dco (DartCObject based), see doc to use other codecs
+    return raw as List<double>;
+  }
+
+  @protected
+  Float32List dco_decode_list_prim_f_32_strict(dynamic raw) {
+    // Codec=Dco (DartCObject based), see doc to use other codecs
+    return raw as Float32List;
+  }
+
+  @protected
+  Uint64List dco_decode_list_prim_u_64_strict(dynamic raw) {
+    // Codec=Dco (DartCObject based), see doc to use other codecs
+    return dcoDecodeUint64List(raw);
+  }
+
+  @protected
   Uint8List dco_decode_list_prim_u_8_strict(dynamic raw) {
     // Codec=Dco (DartCObject based), see doc to use other codecs
     return raw as Uint8List;
+  }
+
+  @protected
+  (
+    Uint64List,
+    Float32List
+  ) dco_decode_record_list_prim_u_64_strict_list_prim_f_32_strict(dynamic raw) {
+    // Codec=Dco (DartCObject based), see doc to use other codecs
+    final arr = raw as List<dynamic>;
+    if (arr.length != 2) {
+      throw Exception('Expected 2 elements, got ${arr.length}');
+    }
+    return (
+      dco_decode_list_prim_u_64_strict(arr[0]),
+      dco_decode_list_prim_f_32_strict(arr[1]),
+    );
+  }
+
+  @protected
+  (BigInt, BigInt, BigInt, BigInt, BigInt)
+      dco_decode_record_usize_usize_usize_usize_usize(dynamic raw) {
+    // Codec=Dco (DartCObject based), see doc to use other codecs
+    final arr = raw as List<dynamic>;
+    if (arr.length != 5) {
+      throw Exception('Expected 5 elements, got ${arr.length}');
+    }
+    return (
+      dco_decode_usize(arr[0]),
+      dco_decode_usize(arr[1]),
+      dco_decode_usize(arr[2]),
+      dco_decode_usize(arr[3]),
+      dco_decode_usize(arr[4]),
+    );
+  }
+
+  @protected
+  BigInt dco_decode_u_64(dynamic raw) {
+    // Codec=Dco (DartCObject based), see doc to use other codecs
+    return dcoDecodeU64(raw);
   }
 
   @protected
@@ -152,6 +446,12 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   }
 
   @protected
+  BigInt dco_decode_usize(dynamic raw) {
+    // Codec=Dco (DartCObject based), see doc to use other codecs
+    return dcoDecodeU64(raw);
+  }
+
+  @protected
   String sse_decode_String(SseDeserializer deserializer) {
     // Codec=Sse (Serialization based), see doc to use other codecs
     var inner = sse_decode_list_prim_u_8_strict(deserializer);
@@ -159,10 +459,79 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   }
 
   @protected
+  double sse_decode_f_32(SseDeserializer deserializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    return deserializer.buffer.getFloat32();
+  }
+
+  @protected
+  List<Float32List> sse_decode_list_list_prim_f_32_strict(
+      SseDeserializer deserializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+
+    var len_ = sse_decode_i_32(deserializer);
+    var ans_ = <Float32List>[];
+    for (var idx_ = 0; idx_ < len_; ++idx_) {
+      ans_.add(sse_decode_list_prim_f_32_strict(deserializer));
+    }
+    return ans_;
+  }
+
+  @protected
+  List<double> sse_decode_list_prim_f_32_loose(SseDeserializer deserializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    var len_ = sse_decode_i_32(deserializer);
+    return deserializer.buffer.getFloat32List(len_);
+  }
+
+  @protected
+  Float32List sse_decode_list_prim_f_32_strict(SseDeserializer deserializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    var len_ = sse_decode_i_32(deserializer);
+    return deserializer.buffer.getFloat32List(len_);
+  }
+
+  @protected
+  Uint64List sse_decode_list_prim_u_64_strict(SseDeserializer deserializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    var len_ = sse_decode_i_32(deserializer);
+    return deserializer.buffer.getUint64List(len_);
+  }
+
+  @protected
   Uint8List sse_decode_list_prim_u_8_strict(SseDeserializer deserializer) {
     // Codec=Sse (Serialization based), see doc to use other codecs
     var len_ = sse_decode_i_32(deserializer);
     return deserializer.buffer.getUint8List(len_);
+  }
+
+  @protected
+  (Uint64List, Float32List)
+      sse_decode_record_list_prim_u_64_strict_list_prim_f_32_strict(
+          SseDeserializer deserializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    var var_field0 = sse_decode_list_prim_u_64_strict(deserializer);
+    var var_field1 = sse_decode_list_prim_f_32_strict(deserializer);
+    return (var_field0, var_field1);
+  }
+
+  @protected
+  (BigInt, BigInt, BigInt, BigInt, BigInt)
+      sse_decode_record_usize_usize_usize_usize_usize(
+          SseDeserializer deserializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    var var_field0 = sse_decode_usize(deserializer);
+    var var_field1 = sse_decode_usize(deserializer);
+    var var_field2 = sse_decode_usize(deserializer);
+    var var_field3 = sse_decode_usize(deserializer);
+    var var_field4 = sse_decode_usize(deserializer);
+    return (var_field0, var_field1, var_field2, var_field3, var_field4);
+  }
+
+  @protected
+  BigInt sse_decode_u_64(SseDeserializer deserializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    return deserializer.buffer.getBigUint64();
   }
 
   @protected
@@ -174,6 +543,12 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   @protected
   void sse_decode_unit(SseDeserializer deserializer) {
     // Codec=Sse (Serialization based), see doc to use other codecs
+  }
+
+  @protected
+  BigInt sse_decode_usize(SseDeserializer deserializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    return deserializer.buffer.getBigUint64();
   }
 
   @protected
@@ -195,11 +570,77 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   }
 
   @protected
+  void sse_encode_f_32(double self, SseSerializer serializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    serializer.buffer.putFloat32(self);
+  }
+
+  @protected
+  void sse_encode_list_list_prim_f_32_strict(
+      List<Float32List> self, SseSerializer serializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    sse_encode_i_32(self.length, serializer);
+    for (final item in self) {
+      sse_encode_list_prim_f_32_strict(item, serializer);
+    }
+  }
+
+  @protected
+  void sse_encode_list_prim_f_32_loose(
+      List<double> self, SseSerializer serializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    sse_encode_i_32(self.length, serializer);
+    serializer.buffer.putFloat32List(
+        self is Float32List ? self : Float32List.fromList(self));
+  }
+
+  @protected
+  void sse_encode_list_prim_f_32_strict(
+      Float32List self, SseSerializer serializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    sse_encode_i_32(self.length, serializer);
+    serializer.buffer.putFloat32List(self);
+  }
+
+  @protected
+  void sse_encode_list_prim_u_64_strict(
+      Uint64List self, SseSerializer serializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    sse_encode_i_32(self.length, serializer);
+    serializer.buffer.putUint64List(self);
+  }
+
+  @protected
   void sse_encode_list_prim_u_8_strict(
       Uint8List self, SseSerializer serializer) {
     // Codec=Sse (Serialization based), see doc to use other codecs
     sse_encode_i_32(self.length, serializer);
     serializer.buffer.putUint8List(self);
+  }
+
+  @protected
+  void sse_encode_record_list_prim_u_64_strict_list_prim_f_32_strict(
+      (Uint64List, Float32List) self, SseSerializer serializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    sse_encode_list_prim_u_64_strict(self.$1, serializer);
+    sse_encode_list_prim_f_32_strict(self.$2, serializer);
+  }
+
+  @protected
+  void sse_encode_record_usize_usize_usize_usize_usize(
+      (BigInt, BigInt, BigInt, BigInt, BigInt) self, SseSerializer serializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    sse_encode_usize(self.$1, serializer);
+    sse_encode_usize(self.$2, serializer);
+    sse_encode_usize(self.$3, serializer);
+    sse_encode_usize(self.$4, serializer);
+    sse_encode_usize(self.$5, serializer);
+  }
+
+  @protected
+  void sse_encode_u_64(BigInt self, SseSerializer serializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    serializer.buffer.putBigUint64(self);
   }
 
   @protected
@@ -211,6 +652,12 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   @protected
   void sse_encode_unit(void self, SseSerializer serializer) {
     // Codec=Sse (Serialization based), see doc to use other codecs
+  }
+
+  @protected
+  void sse_encode_usize(BigInt self, SseSerializer serializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    serializer.buffer.putBigUint64(self);
   }
 
   @protected
